52a06ac0a4419423336447962f3445210fd3b7b9
Eachan Johnson <eachan.johnson@crick.ac.uk>
2024-07-31 11:07:19 +0100

    Clean up code for readability and maintainability

--- rf2t_micro/resnet.py
+++ rf2t_micro/resnet.py
@@ -1,82 +1,84 @@
+"""Code 2D-convolutional residual blocks and networks."""
+
 import torch
-import torch.nn as nn
+from torch.nn import Conv2d, ELU, Dropout, InstanceNorm2d, Module, Sequential
+
+def _get_same_padding(kernel: int, dilation: int):
+    return (kernel + (kernel - 1) * (dilation - 1) - 1) // 2
 
 # original resblock
-class ResBlock2D(nn.Module):
-    def __init__(self, n_c, kernel=3, dilation=1, p_drop=0.15):
-        super(ResBlock2D, self).__init__()
-        padding = self._get_same_padding(kernel, dilation)
+class ResBlock2D(Module):
+    def __init__(self, n_c, 
+                 kernel: int = 3, 
+                 dilation: int = 1, 
+                 p_drop: float = 0.15):
+        super().__init__()
+        padding = _get_same_padding(kernel, dilation)
 
-        layer_s = list()
-        layer_s.append(nn.Conv2d(n_c, n_c, kernel, padding=padding, dilation=dilation, bias=False))
-        layer_s.append(nn.InstanceNorm2d(n_c, affine=True, eps=1e-6))
-        layer_s.append(nn.ELU(inplace=True))
-        # dropout
-        layer_s.append(nn.Dropout(p_drop))
-        # convolution
-        layer_s.append(nn.Conv2d(n_c, n_c, kernel, dilation=dilation, padding=padding, bias=False))
-        layer_s.append(nn.InstanceNorm2d(n_c, affine=True, eps=1e-6))
-        self.layer = nn.Sequential(*layer_s)
-        self.final_activation = nn.ELU(inplace=True)
-
-    def _get_same_padding(self, kernel, dilation):
-        return (kernel + (kernel - 1) * (dilation - 1) - 1) // 2
+        layer_s = [
+            Conv2d(n_c, n_c, kernel, padding=padding, dilation=dilation, bias=False),
+            InstanceNorm2d(n_c, affine=True, eps=1e-6),
+            ELU(inplace=True),
+            Dropout(p_drop),
+            Conv2d(n_c, n_c, kernel, dilation=dilation, padding=padding, bias=False),
+            InstanceNorm2d(n_c, affine=True, eps=1e-6)
+        ]
+        self.layer = Sequential(*layer_s)
+        self.final_activation = ELU(inplace=True)
 
     def forward(self, x):
-        out = self.layer(x)
-        return self.final_activation(x + out)
+        return self.final_activation(x + self.layer(x))
+
 
 # pre-activation bottleneck resblock
-class ResBlock2D_bottleneck(nn.Module):
-    def __init__(self, n_c, kernel=3, dilation=1, p_drop=0.15):
-        super(ResBlock2D_bottleneck, self).__init__()
-        padding = self._get_same_padding(kernel, dilation)
-
+class ResBlock2D_bottleneck(Module):
+    def __init__(self, n_c, 
+                 kernel: int = 3, 
+                 dilation: int = 1, 
+                 p_drop: float = 0.15):
+        super().__init__()
+        padding = _get_same_padding(kernel, dilation)
         n_b = n_c // 2 # bottleneck channel
         
-        layer_s = list()
-        # pre-activation
-        layer_s.append(nn.InstanceNorm2d(n_c, affine=True, eps=1e-6))
-        layer_s.append(nn.ELU(inplace=True))
-        # project down to n_b
-        layer_s.append(nn.Conv2d(n_c, n_b, 1, bias=False))
-        layer_s.append(nn.InstanceNorm2d(n_b, affine=True, eps=1e-6))
-        layer_s.append(nn.ELU(inplace=True))
-        # convolution
-        layer_s.append(nn.Conv2d(n_b, n_b, kernel, dilation=dilation, padding=padding, bias=False))
-        layer_s.append(nn.InstanceNorm2d(n_b, affine=True, eps=1e-6))
-        layer_s.append(nn.ELU(inplace=True))
-        # dropout
-        layer_s.append(nn.Dropout(p_drop))
-        # project up
-        layer_s.append(nn.Conv2d(n_b, n_c, 1, bias=False))
-
-        self.layer = nn.Sequential(*layer_s)
-
-    def _get_same_padding(self, kernel, dilation):
-        return (kernel + (kernel - 1) * (dilation - 1) - 1) // 2
+        layer_s = [
+            InstanceNorm2d(n_c, affine=True, eps=1e-6),
+            ELU(inplace=True),
+            Conv2d(n_c, n_b, 1, bias=False),
+            InstanceNorm2d(n_b, affine=True, eps=1e-6),
+            ELU(inplace=True),
+            Conv2d(n_b, n_b, kernel, dilation=dilation, padding=padding, bias=False),
+            InstanceNorm2d(n_b, affine=True, eps=1e-6),
+            ELU(inplace=True),
+            Dropout(p_drop),
+            Conv2d(n_b, n_c, 1, bias=False)
+        ]
+        self.layer = Sequential(*layer_s)
 
     def forward(self, x):
-        out = self.layer(x)
-        return x + out
+        return x + self.layer(x)
 
-class ResidualNetwork(nn.Module):
+
+class ResidualNetwork(Module):
     def __init__(self, n_block, n_feat_in, n_feat_block, n_feat_out, 
-                 dilation=[1,2,4,8], block_type='orig', p_drop=0.15):
-        super(ResidualNetwork, self).__init__()
+                 dilation: Optional[Iterable[int]] = None, 
+                 block_type: str = 'orig', 
+                 p_drop: float = 0.15):
+        super().__init__()
+        if dilation is None:
+            dilation = [1,2,4,8]
 
-
-        layer_s = list()
         # project to n_feat_block
         if n_feat_in != n_feat_block:
-            layer_s.append(nn.Conv2d(n_feat_in, n_feat_block, 1, bias=False))
-            if block_type =='orig': # should acitivate input
-                layer_s.append(nn.InstanceNorm2d(n_feat_block, affine=True, eps=1e-6))
-                layer_s.append(nn.ELU(inplace=True))
+            layer_s = [Conv2d(n_feat_in, n_feat_block, 1, bias=False)]
+            if block_type == 'orig': # should activate input
+                layer_s += [InstanceNorm2d(n_feat_block, affine=True, eps=1e-6),
+                            ELU(inplace=True)]
+        else:
+            layer_s = []
 
         # add resblocks
         for i_block in range(n_block):
-            d = dilation[i_block%len(dilation)]
+            d = dilation[i_block % len(dilation)]
             if block_type == 'orig':
                 res_block = ResBlock2D(n_feat_block, kernel=3, dilation=d, p_drop=p_drop)
             else:
@@ -85,11 +87,10 @@ class ResidualNetwork(nn.Module):
 
         if n_feat_out != n_feat_block:
             # project to n_feat_out
-            layer_s.append(nn.Conv2d(n_feat_block, n_feat_out, 1))
+            layer_s.append(Conv2d(n_feat_block, n_feat_out, 1))
         
-        self.layer = nn.Sequential(*layer_s)
+        self.layer = Sequential(*layer_s)
     
     def forward(self, x):
-        output = self.layer(x)
-        return output
+        return self.layer(x)
 
